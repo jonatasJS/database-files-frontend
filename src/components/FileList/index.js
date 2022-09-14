1b733b504e0e5a2f21947e161528d8d49e348d64
jonatasJS <soaresjonatas398@gmail.com>
2022-09-14 13:48:18 -0300

    asdasda

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -2,69 +2,73 @@ import React, { Component } from "react";
 import * as CircularProgressbar from "react-circular-progressbar";
 import { MdCheckCircle, MdError, MdLink } from "react-icons/md";
 
-import { Container, FileInfo, FileInfoData, Preview } from "./styles";
+import { Container, FileInfo, FileInfoData, ItensStatus, Preview } from "./styles";
 
 export default class FileList extends Component {
   render() {
     return (
       <Container>
-        {this.props.files.map(({
-          id,
-          name,
-          readableSize,
-          preview,
-          progress,
-          uploaded,
-          error,
-          url
-        }) => {
-          return (
-            <li key={`${id}`}>
-              <FileInfo>
-                <Preview src={preview} />
-                <FileInfoData>
-                  <strong>{name}</strong>
-                  <span>
-                    {readableSize}{" "}
-                    {!!url && (
-                      <button onClick={() => this.props.onDelete(id)}>
-                        Excluir
-                      </button>
+        {this.props.files.map(
+          ({
+            id,
+            name,
+            readableSize,
+            preview,
+            progress,
+            uploaded,
+            error,
+            url,
+          }) => {
+            return (
+              <li key={`${id}`}>
+                <FileInfo>
+                  <Preview src={preview} />
+                  <FileInfoData>
+                    <strong>{name}</strong>
+                    <span>
+                      {readableSize}{" "}
+                      {!!url && (
+                        <button onClick={() => this.props.onDelete(id)}>
+                          Excluir
+                        </button>
+                      )}
+                    </span>
+                  </FileInfoData>
+                </FileInfo>
+
+                <main>
+                  {!uploaded && !error ? (
+                    <CircularProgressbar
+                      styles={{
+                        root: { width: 24 },
+                        path: { stroke: "#7159c1" },
+                      }}
+                      strokeWidth={10}
+                      percentage={progress}
+                    />
+                  ) : (
+                    ""
+                  )}
+
+                  <ItensStatus>
+                    {url && (
+                      <a href={url} target="_blank" rel="noopener noreferrer">
+                        <MdLink
+                          style={{ marginRight: 8 }}
+                          size={24}
+                          color="#222"
+                        />
+                      </a>
                     )}
-                  </span>
-                </FileInfoData>
-              </FileInfo>
 
-              <main>
-                {(!uploaded && !error ) ? (
-                  <CircularProgressbar
-                    styles={{
-                      root: { width: 24 },
-                      path: { stroke: "#7159c1" },
-                    }}
-                    strokeWidth={10}
-                    percentage={progress}
-                  />
-                ): ''}
-
-                {url && (
-                  <a
-                    href={url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-                  </a>
-                )}
-
-                {uploaded && (
-                  <MdCheckCircle size={24} color="#78e5d5" />
-                )}
-                {error && <MdError size={24} color="#e57878" />}
-              </main>
-            </li>
-          );
-        })}
+                    {uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+                    {error && <MdError size={24} color="#e57878" />}
+                  </ItensStatus>
+                </main>
+              </li>
+            );
+          }
+        )}
       </Container>
     );
   }
--- src/components/FileList/styles.js
+++ src/components/FileList/styles.js
@@ -1,17 +1,42 @@
 import styled from "styled-components";
 
 export const Container = styled.ul`
+  box-shadow: inset 0 0 5px #000;
+  border-radius: 10px;
   margin-top: 20px;
+  height: 300px;
+  overflow: auto;
+  scroll-behavior: smooth;
 
   li {
+    padding: 15px 10px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     color: #444;
+    outline: 0.5px solid rgba(68, 68, 68, 0.3);
 
-    & + li {
+    /* & + li {
       margin-top: 15px;
-    }
+    } */
+  }
+
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 12px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar {
+    width: 12px;
+    border-radius: 12px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 12px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #555;
   }
 `;
 
@@ -48,4 +73,11 @@ export const Preview = styled.div`
   background-size: cover;
   background-position: 50% 50%;
   margin-right: 10px;
+`;
+
+export const ItensStatus = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
