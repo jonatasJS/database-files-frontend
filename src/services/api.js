68a70861e68d7081ad64c5135439ba78d1f857b0
jonatasJS <soaresjonatas398@gmail.com>
2022-09-15 14:55:34 -0300

    zxczxcxc

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_LOCALHOST_URL ? process.env.REACT_APP_LOCALHOST_URL : "https://database-files-backend.herokuapp.com/",
+  baseURL: process.env.REACT_APP_LOCALHOST_URL == "http://localhost:4000/" ? process.env.REACT_APP_LOCALHOST_URL : "https://database-files-backend.herokuapp.com/",
 });
 
 export default api;
