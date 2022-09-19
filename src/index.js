a42a63945ee942cf49784728e2ea9dd055d1a2a6
jonatasJS <soaresjonatas398@gmail.com>
2022-09-19 18:00:25 -0300

    sdfsdf

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    {console.log(process.env.NODE_ENV)}
     <GlobalStyle />
   </React.StrictMode>
 );
